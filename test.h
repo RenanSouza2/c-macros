f8e1184c31aa6564792b97220ee07b7d8bf2f769
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-04-23 19:36:25 -0300

    mid commit

--- test.h
+++ test.h
@@ -1,6 +1,7 @@
 #ifndef __TEST_REVERT_H__
 #define __TEST_REVERT_H__
 
+#include <stdbool.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -10,11 +11,20 @@
 
 #define TEST_LIB printf("\n%s\t\t", __func__);
 
-#define TEST_FN printf("\n\t%s\t\t", __func__);
+#define TEST_FN_OPEN                \
+    printf("\n\t%s\t\t", __func__); \
+    bool __is_main_process = true;
+
+#define TEST_FN_CLOSE       \
+    TEST_ASSERT_EMPTY       \
+    if(!__is_main_process)  \
+        exit(EXIT_SUCCESS)
 
 #define TEST_CASE_OPEN(TAG)                                                                     \
-    if(show) printf("\n\t\t%s %2d\t\t", __func__, (int)(TAG));                                  \
+    if(__is_main_process)                                                                       \
     {                                                                                           \
+        if(show)                                                                                \
+            printf("\n\t\t%s %2d\t\t", __func__, (int)(TAG));                                   \
         int pid = fork();                                                                       \
         assert(pid >= 0);                                                                       \
         if(pid)                                                                                 \
@@ -29,16 +39,18 @@
         }                                                                                       \
         else                                                                                    \
         {                                                                                       \
-            usleep(0);
+            usleep(0);                                                                          \
+            __is_main_process = false
 
 #define TEST_DEFAULT_CLOSE      \
             exit(EXIT_SUCCESS); \
         }                       \
     }
 
-#define TEST_CASE_CLOSE     \
-        TEST_ASSERT_EMPTY   \
-    TEST_DEFAULT_CLOSE
+#define TEST_CASE_CLOSE         \
+            TEST_ASSERT_EMPTY   \
+        }                       \
+    }
 
 #define TEST_REVERT_OPEN                                        \
     {                                                           \
