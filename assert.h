a0b505eae810edffee364f8f278bff07bba56a3a
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-04-24 22:20:34 -0300

    format

--- assert.h
+++ assert.h
@@ -10,16 +10,14 @@
 
 #define assert(COND)        \
     if(!(COND))             \
-    {                       \
         exit(EXIT_FAILURE); \
-    }
 
 #elif defined __linux__
 
 #define assert(COND)                                                    \
     if(!(COND))                                                         \
     {                                                                   \
-        perror("\n\n");                                                 \
+        fprintf(stderr, "\n\n");                                        \
         __assert_fail(#COND, __FILE__, __LINE__, __ASSERT_FUNCTION);    \
     }
 
@@ -28,7 +26,7 @@
 #define assert(COND)                                                    \
     if(!(COND))                                                         \
     {                                                                   \
-        perror("\n\n");                                                 \
+        fprintf(stderr, "\n\n");                                        \
         __assert_rtn(__func__, __ASSERT_FILE_NAME, __LINE__, #COND);    \
     }
 
