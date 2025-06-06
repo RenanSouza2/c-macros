80b43ebfe229f807190927b8bf591961dab8a75d
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-06-06 12:22:17 -0300

    include signed

--- U64.h
+++ U64.h
@@ -7,11 +7,13 @@
 
 #ifdef __linux__
 
+#define D64P(C) "%" #C "ld"
 #define U64P(C) "%" #C "lu"
 #define U64PX "%016lx"
 
 #elif defined __APPLE__
 
+#define D64P(C) "%" #C "lld"
 #define U64P(C) "%" #C "llu"
 #define U64PX "%016llx"
 
