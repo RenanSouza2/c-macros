0093964d1709e5f33878d047a8af14fa4f915fce
Renan Souza <renan.rodrigues.souza1@gmail.com>
2025-06-30 09:46:50 -0300

    v2 (#2)
    * fixes
    
    * test timeout open default
    
    * mid commit
    
    * mid commit
    
    * mod commit
    
    * incorparete timout in case
    
    * mid commit
    
    * fix bug
    
    * use more appropiate function
    
    * format
    
    * many changes
    
    * skip mem test on revert
    
    * tests
    
    * update logs and wait
    
    * remove to function
    
    * more functions
    
    * test
    
    * fix revert cases
    
    * include signed
    
    * format
    
    * threads macro
    
    * fix macro
    
    * define uint64_p

--- assert.h
+++ assert.h
@@ -10,25 +10,23 @@
 
 #define assert(COND)        \
     if(!(COND))             \
-    {                       \
         exit(EXIT_FAILURE); \
-    }
 
 #elif defined __linux__
 
-#define assert(COND)                \
-    if(!(COND))                     \
-    {                               \
-        fprintf(stderr, "\n\n");    \
+#define assert(COND)                                                    \
+    if(!(COND))                                                         \
+    {                                                                   \
+        fprintf(stderr, "\n\n");                                        \
         __assert_fail(#COND, __FILE__, __LINE__, __ASSERT_FUNCTION);    \
     }
 
 #elif defined __APPLE__
 
-#define assert(COND)                \
-    if(!(COND))                     \
-    {                               \
-        fprintf(stderr, "\n\n");    \
+#define assert(COND)                                                    \
+    if(!(COND))                                                         \
+    {                                                                   \
+        fprintf(stderr, "\n\n");                                        \
         __assert_rtn(__func__, __ASSERT_FILE_NAME, __LINE__, #COND);    \
     }
 
--- test.h
+++ test.h
@@ -1,108 +1,159 @@
-#ifndef __TEST_REVERT_H__
-#define __TEST_REVERT_H__
+#ifndef __TEST_H__
+#define __TEST_H__
 
+#include <stdbool.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <stdarg.h>
 
-#include "./assert.h"
+#include "assert.h"
 
 #define TEST_LIB printf("\n%s\t\t", __func__);
 
-#define TEST_FN printf("\n\t%s\t\t", __func__);
+#define TEST_FN_OPEN                    \
+    {                                   \
+        printf("\n\t%s\t\t", __func__); \
+        bool __is_main_process = true;  \
+        bool __test_memory = true;      \
+        int __tag = 0;
 
-#define TEST_CASE_OPEN(TAG)                                                                     \
-    if(show) printf("\n\t\t%s %2d\t\t", __func__, (int)(TAG));                                  \
-    {                                                                                           \
-        int pid = fork();                                                                       \
-        assert(pid >= 0);                                                                       \
-        if(pid)                                                                                 \
-        {                                                                                       \
-            int status;                                                                         \
-            assert(waitpid(pid, &status, 0) >= 0);                                              \
-            if(status != EXIT_SUCCESS)                                                          \
-            {                                                                                   \
-                printf("\n\n\tERROR TEST\t| l:%d | %s %d\t\t", __LINE__, __func__, (int)(TAG)); \
-                exit(EXIT_FAILURE);                                                             \
-            }                                                                                   \
-        }                                                                                       \
-        else                                                                                    \
-        {                                                                                       \
-            usleep(0);
-
-#define TEST_DEFAULT_CLOSE      \
-            exit(EXIT_SUCCESS); \
-        }                       \
+#define TEST_FN_CLOSE               \
+        if(__test_memory)           \
+            TEST_ASSERT_MEM_EMPTY   \
+        if(!__is_main_process)      \
+            exit(EXIT_SUCCESS);     \
     }
 
-#define TEST_CASE_CLOSE     \
-        TEST_ASSERT_EMPTY   \
-    TEST_DEFAULT_CLOSE
+void test_log_error(int __tag, int line, char const func[], char format[], ...)
+{
+    va_list args;
+    va_start(args, format);
+    printf("\n\n\tERROR TEST\t| l:%d | %s %d | ", line, func, __tag);
+    vprintf(format, args);
+    printf("\n\n");
+    exit(EXIT_FAILURE);
+}
 
-#define TEST_REVERT_OPEN                                        \
-    {                                                           \
-        int pid = fork();                                       \
-        assert(pid >= 0);                                       \
-        if(pid)                                                 \
-        {                                                       \
-            int status;                                         \
-            assert(waitpid(pid, &status, 0) >= 0);              \
-            assert(status != EXIT_SUCCESS);                     \
-        }                                                       \
-        else                                                    \
-        {                                                       \
-            if(                                                 \
-                freopen("/dev/null", "w", stdout) == NULL ||    \
-                freopen("/dev/null", "w", stderr) == NULL       \
-            )                                                   \
-            {                                                   \
-                exit(EXIT_SUCCESS);                             \
-            }                                                   \
-            usleep(0);
+pid_t fork_assert(int __tag, int line, char const func[], char fork_tag[])
+{
+    pid_t pid = fork();
+    if(pid < 0)
+    {
+        test_log_error(__tag, line, func, "FORKING %s", fork_tag);
+    }
+    return pid;
+}
 
-#define TEST_REVERT_CLOSE TEST_DEFAULT_CLOSE
+// returns true if main process
+bool start_case(int __tag, int line, char const func[], bool show)
+{
+    if(show)
+        printf("\n\t\t%s %2d\t\t", func, __tag);
 
-#define TEST_TIMEOUT_DEFAULT 5
+    pid_t pid = fork_assert(__tag, line, func, "TEST");
+    if(pid)
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        assert(status == EXIT_SUCCESS);
+        return true;
+    }
 
-#define TEST_TIMEOUT_OPEN(TIMEOUT)                      \
-    {                                                   \
-        pid_t pid_test = fork();                        \
-        assert(pid_test >= 0);                          \
-        if(pid_test)                                    \
-        {                                               \
-            pid_t pid_timeout = fork();                 \
-            assert(pid_timeout >= 0);                   \
-            if(pid_timeout < 0)                         \
-            {                                           \
-                kill(pid_test, SIGKILL);                \
-                exit(EXIT_FAILURE);                     \
-            }                                           \
-            if(pid_timeout == 0)                        \
-            {                                           \
-                sleep(TIMEOUT);                         \
-                exit(EXIT_SUCCESS);                     \
-            }                                           \
-            int status;                                 \
-            pid_t pid_return = waitpid(0, &status, 0);  \
-            assert(pid_return > 0);                     \
-            if(pid_return == pid_timeout)               \
-            {                                           \
-                kill(pid_test, SIGKILL);                \
-                printf("\n\n\tTest timeout\n\n");       \
-                exit(EXIT_FAILURE);                     \
-            }                                           \
-            kill(pid_timeout, SIGKILL);                 \
-            if(status)                                  \
-            {                                           \
-                printf("\n\n\tTest faillure\n\n");      \
-                exit(EXIT_FAILURE);                     \
-            }                                           \
-        }                                               \
-        else                                            \
+    int pid_test = fork_assert(__tag, line, func, "TEST");
+    if(pid_test == 0)
+        return false;
+
+    pid_t pid_timeout = fork_assert(__tag, line, func, "TIMEOUT");
+    if(pid_timeout == 0)
+    {
+        usleep(TEST_CASE_TIMEOUT_MS * 1000);
+        exit(EXIT_SUCCESS);
+    }
+
+    int status;
+    pid_t pid_return = waitpid(0, &status, 0);
+    if(pid_return < 0)
+    {
+        test_log_error(__tag, line, func, "WAITPID RETURNED %d", pid_return);
+    }
+
+    if(pid_return == pid_timeout)
+    {
+        kill(pid_test, SIGKILL);
+        test_log_error(__tag, line, func, "TEST TIMEOUT");
+    }
+
+    if(pid_return != pid_test)
+    {
+        test_log_error(__tag, line, func, "INVALID PID CAUGHT %d", pid_return);
+    }
+
+    kill(pid_timeout, SIGKILL);
+    if(status != EXIT_SUCCESS)
+    {
+        test_log_error(__tag, line, func, "ERROR IN TEST EXECUTION ");
+    }
+    exit(EXIT_SUCCESS);
+}
+
+#define TEST_CASE_OPEN(TAG)                                                 \
+    if(__is_main_process)                                                   \
+    {                                                                       \
+        __tag = (int)(TAG);                                                 \
+        __is_main_process = start_case(__tag, __LINE__, __func__, show);    \
+        if(!__is_main_process)                                              \
         {
 
-#define TEST_TIMEOUT_CLOSE TEST_DEFAULT_CLOSE
+#define TEST_CASE_CLOSE \
+        }               \
+    }
+
+pid_t start_revert(int __tag, int line, char const func[])
+{
+    int pid = fork();
+    if(pid < 0)
+    {
+        test_log_error(__tag, line, func, "ERROR FORKING");
+    }
+    if(pid)
+    {
+        int status;
+        if(waitpid(pid, &status, 0) < 0)
+        {
+            test_log_error(__tag, line, func, "WAITPID RETURNED INVALID");
+        }
+        if(status == EXIT_SUCCESS)
+        {
+            test_log_error(__tag, line, func, "TEST EXPECTED TO REVERT BUT DIDN'T");
+        }
+    }
+    else
+    {
+        if(
+            freopen("/dev/null", "w", stderr) == NULL ||
+            freopen("/dev/null", "w", stdout) == NULL
+        )
+        {
+            printf("\n\n\tERROR REDIRECTING STD BUFFERS\n\n");
+            exit(EXIT_SUCCESS);
+        }
+        usleep(0);
+    }
+    return pid;
+}
+
+#define TEST_REVERT_OPEN                                    \
+    {                                                       \
+        if(start_revert(__tag, __LINE__, __func__) == 0)    \
+        {
+
+#define TEST_REVERT_CLOSE       \
+            exit(EXIT_SUCCESS); \
+        }                       \
+        __test_memory = false;  \
+    }
 
 #define ARG_OPEN(...) __VA_ARGS__
 
