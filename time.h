178cf2ad1487df6378ed7afbb33bf6caccc71af6
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-07-11 19:13:15 -0300

    miprove time

--- time.h
+++ time.h
@@ -5,25 +5,22 @@
 
 #include "U64.h"
 
-uint64_t get_time();
+#define TIME_BEGIN _time_begin = get_time();
 
-#define TIME_SETUP uint64_t _time_begin, _time_end;
-
-#define TIME_START _time_begin = get_time();
+#define TIME_SETUP                      \
+    uint64_t _time_begin, _time_end;    \
+    TIME_BEGIN
 
 #define TIME_END(TIME_VAR)                          \
     _time_end = get_time();                         \
     uint64_t TIME_VAR = _time_end - _time_begin;    \
+    _time_begin = _time_end;
 
-#ifndef SKIP_TIME
-
-uint64_t get_time()
+static uint64_t get_time()
 {
     struct timespec time;
     clock_gettime(CLOCK_MONOTONIC_RAW, &time);
     return time.tv_sec * (uint64_t)1e9 + time.tv_nsec;
 }
-
-#endif
 
 #endif
