3d2528d227e4387cfc3a0703bb9e5a959499d305
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-07-30 18:02:25 -0300

    better uint macros

--- uint.h
+++ uint.h
@@ -3,17 +3,23 @@
 
 #include <stdint.h>
 
+typedef uint64_t * uint64_p;
+typedef __uint128_t uint128_t;
+typedef __int128_t int128_t;
+
 #define U64(VALUE) ((uint64_t)(VALUE))
 #define U128(V) ((uint128_t)(V))
-#define U128_IMMED(V1, V2) ((U128(V1) << 64) | (V2))
+#define I128(V) ((int128_t)(V))
+#define U128HL(V1, V2) ((U128(V1) << 64) | (V2))
 #define MUL(V1, V2) U128(V1) * U128(V2)
 #define LOW(V) U64(V)
 #define HIGH(V) U64((V) >> 64)
 #define B(BITS) (1UL << (BITS))
+#define B128(BITS) (U128(1UL) << (BITS))
 
-typedef uint64_t * uint64_p;
-typedef __uint128_t uint128_t;
-typedef __int128_t int128_t;
+#define UINT128_MAX U128HL(UINT64_MAX, UINT64_MAX)
+#define INT128_MAX I128(U128HL(UINT64_MAX / 2, UINT64_MAX))
+#define INT128_MIN I128(U128HL(B(63), 0))
 
 #ifdef __linux__
 
@@ -28,5 +34,8 @@ typedef __int128_t int128_t;
 #define U64PX "%016llx"
 
 #endif
+
+#define U128PX U64P U64P
+#define U128A(C) HIGH(C), LOW(C)
 
 #endif
