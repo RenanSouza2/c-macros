28261ab2bb1944c821b159fe6ebf6dbd14b1e0b0
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-08-29 08:35:58 -0300

    fix time

--- time.h
+++ time.h
@@ -16,11 +16,11 @@
     uint64_t TIME_VAR = _time_end - _time_begin;    \
     _time_begin = _time_end;
 
-static uint64_t get_time()
+static uint64_t get_time(void)
 {
     struct timespec time;
     clock_gettime(CLOCK_MONOTONIC_RAW, &time);
-    return time.tv_sec * (uint64_t)1e9 + time.tv_nsec;
+    return (uint64_t)time.tv_sec * (uint64_t)1e9 + (uint64_t)time.tv_nsec;
 }
 
 #endif
