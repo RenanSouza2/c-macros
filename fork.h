c4e36f3ac98415cfa574d32c1bcf4665b882f0c3
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-02-13 19:45:22 -0300

    fork.h

---
+++ fork.h
@@ -0,0 +1,33 @@
+#ifndef __MACROS_FORK_H__
+#define __MACROS_FORK_H__
+
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "uint.h"
+
+__attribute__((unused))
+static pid_t fork_safe(void)
+{
+    pid_t pid = fork();
+    if(pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    return pid;
+}
+
+__attribute__((unused))
+static pid_t waitpid_safe(pid_t pid, int *status)
+{
+    pid_t pid_return = waitpid(pid, status, 0);
+    if(pid_return <= 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    return pid_return;
+}
+
+#endif
