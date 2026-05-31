989c0490d66fa2463fe695f047061c626823eb9c
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-05-31 19:14:50 -0300

    fix warnings

--- time.h
+++ time.h
@@ -5,10 +5,13 @@
 
 #include "uint.h" // IWYU pragma: keep
 
+constexpr uint64_t nanosecs_in_sec = 1'000'000'000;
+
 #define TIME_RESET _time_begin = get_time();
 
-#define TIME_SETUP                      \
-    uint64_t _time_begin, _time_end;    \
+#define TIME_SETUP          \
+    uint64_t _time_begin;   \
+    uint64_t _time_end;     \
     TIME_RESET
 
 #define TIME_END(TIME_VAR)                          \
@@ -27,7 +30,7 @@ static uint64_t get_time()
 {
     struct timespec time;
     clock_gettime(CLU_CLOCK_ID, &time);
-    return ((uint64_t)time.tv_sec * 1000000000ULL) + (uint64_t)time.tv_nsec;
+    return ((uint64_t)time.tv_sec * nanosecs_in_sec) + (uint64_t)time.tv_nsec;
 }
 
 [[nodiscard, maybe_unused]]
