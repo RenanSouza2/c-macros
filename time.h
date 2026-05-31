e39577659b42ae2bf68ccf7ad1d291d1f401dd3d
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-05-31 19:15:48 -0300

    Revert "Merge remote-tracking branch 'origin/v3.3' into v3.3"
    This reverts commit 8b2b2cb31e15bd480dc3445fdcfdfb5811a40f18, reversing
    changes made to 989c0490d66fa2463fe695f047061c626823eb9c.

--- time.h
+++ time.h
@@ -30,19 +30,13 @@ static uint64_t get_time()
 {
     struct timespec time;
     clock_gettime(CLU_CLOCK_ID, &time);
-<<<<<<< HEAD
     return ((uint64_t)time.tv_sec * nanosecs_in_sec) + (uint64_t)time.tv_nsec;
-=======
-    constexpr uint64_t nanoseconds_per_second = 1'000'000'000;
-    return ((uint64_t)time.tv_sec * nanoseconds_per_second) + (uint64_t)time.tv_nsec;
->>>>>>> origin/v3.3
 }
 
 [[nodiscard, maybe_unused]]
 static double dtime(uint64_t t)
 {
-    constexpr uint64_t nanoseconds_per_second = 1'000'000'000;
-    return (double)t / nanoseconds_per_second;
+    return (double)t / 1e9;
 }
 
 #endif
