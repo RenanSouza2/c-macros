9154a7d2616c1590f8e62620ecfcb8bc4395a277
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-08-10 13:07:16 -0300

    fork lock thread

--- fork.h
+++ fork.h
@@ -1,8 +1,17 @@
 #ifndef MACROS_FORK_H
 #define MACROS_FORK_H
 
+#include <stdint.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#if defined(__linux__)
+#include <sched.h>
+#elif defined(__APPLE__)
+#include <mach/mach.h>
+#include <mach/thread_policy.h>
+#include <pthread.h>
+#endif
 
 #include "assert.h"
 
@@ -31,6 +40,25 @@ static pid_t waitpid_safe(pid_t pid, int *status)
     }
 
     return pid_return;
+}
+
+[[maybe_unused]]
+static void fork_lock_processor([[maybe_unused]] uint64_t index)
+{
+#if defined(__linux__)
+    cpu_set_t cpu_set;
+    CPU_ZERO(&cpu_set);
+    CPU_SET(index, &cpu_set);
+    assert(sched_setaffinity(0, sizeof(cpu_set), &cpu_set) == 0);
+#elif defined(__APPLE__)
+    thread_affinity_policy_data_t policy = { .affinity_tag = (integer_t)index };
+    thread_policy_set(
+        pthread_mach_thread_np(pthread_self()),
+        THREAD_AFFINITY_POLICY,
+        (thread_policy_t)&policy,
+        THREAD_AFFINITY_POLICY_COUNT
+    );
+#endif
 }
 
 #endif
